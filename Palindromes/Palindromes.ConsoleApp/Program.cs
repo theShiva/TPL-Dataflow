68d310b85bf6bd1efb36534936f0ad0e57dcef9b
Shiva Manjunath <theblogdoctor@gmail.com>
2017-04-20 23:00:44 -0700

    Wrote downloadString TransformBlock to take a url as parameter and download and return the content as a string.

--- Palindromes/Palindromes.ConsoleApp/Program.cs
+++ Palindromes/Palindromes.ConsoleApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace Palindromes.ConsoleApp
 {
@@ -10,6 +13,44 @@ namespace Palindromes.ConsoleApp
     {
         static void Main(string[] args)
         {
+            // 
+            // Create members of the Pipeline
+            //
+
+            // Download the requested resource as a string
+
+            var downloadString = new TransformBlock<string, string>
+                ( url =>
+                    {
+                        Console.WriteLine($"Downloading from {url}...");
+                        string result = null;
+                        using (var client = new HttpClient())
+                        {
+                            // Perform a synchronous call by calling .Result
+                            var response = client.GetAsync(url).Result;
+
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var responseContent = response.Content;
+
+                                // read result synchronously by calling .Result 
+                                result = responseContent.ReadAsStringAsync().Result;
+                                if (!string.IsNullOrEmpty(result))
+                                    Console.WriteLine($"Downloaded {result.Length} characters...");
+
+                            }
+                        }
+                        return result;
+                    }
+                );
+
+            // Process "The Adventurous Life of a Versatile Artist: Houdini" by by Harry Houdini.
+            downloadString.Post("http://www.gutenberg.org/cache/epub/45370/pg45370.txt");
+
+            downloadString.Complete();
+
+            Console.WriteLine("Press a key to exit:");
+            Console.ReadKey();
         }
     }
 }
